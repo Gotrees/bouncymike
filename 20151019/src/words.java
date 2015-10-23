889674a1ea668200a8b13132677d5a8ff57a0ec7
gotrees <photoms1@aol.com>
2015-10-22 19:45:19 -0700

    game over

--- 20151019/src/words.java
+++ 20151019/src/words.java
@@ -8,6 +8,8 @@ public class words extends GameDriver{
 
 	Rectangle r1 = new Rectangle(10,10,20,20);
 	Rectangle bg = new Rectangle(0,0,600,600);
+	Rectangle r2 = new Rectangle(50,20,300,300);
+	Rectangle r3 = new Rectangle(0,0,600,600);
 	Color c1 = new Color((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
 	int dx = 0, dy = 0;
 	int gravity = 1;
@@ -44,18 +46,38 @@ public class words extends GameDriver{
 			// win.setColor(c1);
 			// win.fill(r1);
 		}
+	
 		r1.translate(dx, dy);
 		win.setColor(c1);
 		 win.fill(r1);
+			win.drawImage(swamp, null, (int)bg.getX(), (int)bg.getY());
 		win.drawImage(mike, null, (int)r1.getX(), (int)r1.getY());
 		if(r1.getX()>700){
 			win.drawString("RIP SULLEY", 300, 300);
+			win.drawString("high schore:"+(int)(Math.random()*100+1), 289, 313);
+			bg.translate(0, -2);
 		}
-
+		int n = 0;
+		if(n>10){
+			c1= new Color((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
+			win.setColor(c1);
+			win.fill(r3);
+		}
+		if(bg.getY()<0){
+			win.setColor(Color.BLACK);
+			win.drawString("GAME OVER", (int)(Math.random()*600), (int)(Math.random()*600));
+			n = n+1;
+		}
+		
+	
+	
 	}
+	BufferedImage swamp = null;
 	BufferedImage mike = null;
 	//constructor
 	public words()/*fdsaikillmeashfldsakjhflsajd*/{
+		swamp = this.addImage("swamp.jpg");
 		mike = this.addImage("mike.jpg");
 	}
+
 }
